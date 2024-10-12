326be22c5ca1720faee10a3e9917d1cd111a1c0f
Ashwin Umadi <ashwinumadi@Ashwins-MacBook-Pro.local>
2024-10-12 15:39:19 -0600

    6

--- test_lite.sh
+++ test_lite.sh
@@ -5,7 +5,7 @@
 #SBATCH --time=24:00:00
 #SBATCH --partition=blanca-curc-gpu
 #SBATCH --gres=gpu:1
-#SBATCH --output=lite_base-%j.out
+#SBATCH --output=lite_test_600-%j.out
 #SBATCH --mail-type="ALL"
 #SBATCH --mail-user="asum8093@colorado.edu"
 
@@ -30,8 +30,8 @@ conda activate py38-pt1131-cuda117
 echo "== This is the scripting step! =="
 
 #python lite.py --data_dir './data/processed_data/' --output_dir './output' --train_batch_size 4 --num_train_epochs 2000 --margin 0.1 --save_epochs 200 --learning_rate 1e-6 --lamb 0.05
-python eval.py --model_dir "./output/02_11_22_Oct_11_2024_batch4_margin0.1_lr1e-06lambda0.05/epochs400" --eval_data_path "./data/processed_data/test_processed.json" --type_vocab_file "./data/processed_data/types.txt" --batch 64
+python eval.py --model_dir "./output/02_11_22_Oct_11_2024_batch4_margin0.1_lr1e-06lambda0.05/epochs600" --eval_data_path "./data/processed_data/test_processed.json" --type_vocab_file "./data/processed_data/types.txt" --batch 64
 
-python result.py  --test "./output/02_11_22_Oct_11_2024_batch4_margin0.1_lr1e-06lambda0.05/epochs400/Evaluation_test_processed.json"  --model_dir "./output/02_11_22_Oct_11_2024_batch4_margin0.1_lr1e-06lambda0.05/epochs400" --threshold_step 0.05
+python result.py  --test "./output/02_11_22_Oct_11_2024_batch4_margin0.1_lr1e-06lambda0.05/epochs600/Evaluation_test_processed.json"  --model_dir "./output/02_11_22_Oct_11_2024_batch4_margin0.1_lr1e-06lambda0.05/epochs600" --threshold_step 0.05
 
 echo "== End of Job =="
